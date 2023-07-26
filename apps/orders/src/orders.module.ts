e6e3e801f71930f86eb739c687c6a15ecddbc77a
MohsenKhashei <m.khasheii@gmail.com>
2023-07-26 13:54:55 +0300

    adding order repository and config

--- apps/orders/src/orders.module.ts
+++ apps/orders/src/orders.module.ts
@@ -4,6 +4,9 @@ import { OrdersService } from './orders.service';
 import { ConfigModule } from '@nestjs/config';
 import * as Joi from 'joi';
 import { DatabaseModule } from '@app/common';
+import { OrdersRepository } from './orders.repository';
+import { MongooseModule } from '@nestjs/mongoose';
+import { Order, OrderSchema } from './schemas/order.schema';
 
 @Module({
   imports: [
@@ -15,8 +18,9 @@ import { DatabaseModule } from '@app/common';
       envFilePath: './apps/orders/.env',
     }),
     DatabaseModule,
+    MongooseModule.forFeature([{ name: Order.name, schema: OrderSchema }]),
   ],
   controllers: [OrdersController],
-  providers: [OrdersService],
+  providers: [OrdersService, OrdersRepository],
 })
 export class OrdersModule {}

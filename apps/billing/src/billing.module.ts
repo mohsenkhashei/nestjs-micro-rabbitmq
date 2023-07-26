9f246ec567157793d85bc9337cedbfa252d84784
MohsenKhashei <m.khasheii@gmail.com>
2023-07-26 21:56:13 +0300

    fixing typo

--- apps/billing/src/billing.module.ts
+++ apps/billing/src/billing.module.ts
@@ -1,13 +1,13 @@
 import { Module } from '@nestjs/common';
 import { BillingController } from './billing.controller';
 import { BillingService } from './billing.service';
-import { RmpModule } from '@app/common';
+import { RmqModule } from '@app/common';
 import { ConfigModule } from '@nestjs/config';
 import * as Joi from 'joi';
 
 @Module({
   imports: [
-    RmpModule,
+    RmqModule,
     ConfigModule.forRoot({
       isGlobal: true,
       validationSchema: Joi.object({

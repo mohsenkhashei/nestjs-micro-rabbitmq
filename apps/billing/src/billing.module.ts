80fdad7fd2cf14f502592ac6a2f1efc2b083a2ec
MohsenKhashei <m.khasheii@gmail.com>
2023-07-28 13:06:05 +0300

    add authentication

--- apps/billing/src/billing.module.ts
+++ apps/billing/src/billing.module.ts
@@ -1,7 +1,7 @@
 import { Module } from '@nestjs/common';
 import { BillingController } from './billing.controller';
 import { BillingService } from './billing.service';
-import { RmqModule } from '@app/common';
+import { AuthModule, RmqModule } from '@app/common';
 import { ConfigModule } from '@nestjs/config';
 import * as Joi from 'joi';
 
@@ -16,6 +16,7 @@ import * as Joi from 'joi';
       }),
       envFilePath: [`./apps/billing/.env`],
     }),
+    AuthModule,
   ],
   controllers: [BillingController],
   providers: [BillingService],
